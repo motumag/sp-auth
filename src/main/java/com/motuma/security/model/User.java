0b87a4068832cfed66bf73f6d13c69538e44833d
Gemechis Doja Gemeda <gemechis.doja@coopbankoromiasc.com>
2024-01-06 17:48:37 +0300

    spring boot 3 jwt auth app

---
+++ src/main/java/com/motuma/security/model/User.java
@@ -0,0 +1,17 @@
+package com.motuma.security.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Long id;
+    private String userName;
+    private String password;
+    private String confirmPassword;
+}
